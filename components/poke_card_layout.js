d22b554ad6953a1a3e929d723bd134c88f543b24
Franreno <franreno4@gmail.com>
2021-08-10 23:31:14 -0300

    Evolution now working, kind of

--- components/poke_card_layout.js
+++ components/poke_card_layout.js
@@ -4,13 +4,6 @@ import Image from 'next/image';
 
 export default function PokeCardLayout(pokeObj) {
 
-    console.log(pokeObj.name)
-
-    // const pokemon = getPokemon(pokeObj.name);
-
-
-    // console.log(pokemon)
-
     return (
         <div className="Card">
             <Link href={`/${pokeObj.name.name}`}>
--- pages/[name].js
+++ pages/[name].js
@@ -2,42 +2,47 @@ import Image from "next/image";
 import Layout from '../components/layout';
 import Link from "next/dist/client/link";
 import "../lib/pokemon";
-
 import { findEvolutionChainArray, findTypesInformation } from "../lib/pokemon";
 
-export const getStaticPaths = async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=898');
+
+export async function getStaticPaths() {
+  const countRes = await fetch('https://pokeapi.co/api/v2/pokemon/')
+  const countData = await countRes.json()
+  const count = countData.count
+
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${count}/`);
   const data = await res.json();
-  const pokemons = data.results;
 
-  const paths = pokemons.map(pokemon => {
-    return {
-      params: { name: pokemon.name }
-    }
-  })
+  const paths = data.results.map((pokemon) => ({
+    params: { name: pokemon.name },
+  }))
 
-  return {
-    paths,
-    fallback: false
-  }
+  return { paths, fallback: false }
 }
 
 
-export const getStaticProps = async (context) => {
-  const name = context.params.name;
-  let res = await fetch('https://pokeapi.co/api/v2/pokemon/'+name);
-  const pokemon = await res.json();
-  res = await fetch('https://pokeapi.co/api/v2/pokemon-species/'+name);
-  const specie = await res.json();
-  res = await fetch(specie.evolution_chain.url);
-  const evolution = await res.json();
-  const evolutionChainPokemons = await findEvolutionChainArray(evolution);
-  const types =  await findTypesInformation(pokemon.types);
+export async function getStaticProps({ params }) {
 
-  return{
-    props: { types: types, evolutionChainPokemons: evolutionChainPokemons, pokemon: pokemon}
+  // console.log(params)
+
+  let resPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`);
+  const pokemon = await resPokemon.json();
+
+  let resSpecies = await fetch(pokemon.species.url);
+  const specie = await resSpecies.json();
+
+  let resEvolution = await fetch(specie.evolution_chain.url);
+  const evolution = await resEvolution.json();
+
+  const evolutionChainPokemons = await findEvolutionChainArray(evolution);
+  const types = await findTypesInformation(pokemon.types);
+
+  return {
+    props: { types: types, evolutionChainPokemons: evolutionChainPokemons, pokemon: pokemon }
 
   }
+
+
 
 }
 
@@ -45,24 +50,24 @@ const Detalhes = (props) => {
   return (
     <Layout>
       <div>
-       <h1>{props.pokemon.name}</h1>
-        <p>Tipo: {props.types.map(type=>{
-                    return (type.name+' ');
+        <h1>{props.pokemon.name}</h1>
+        <p>Tipo: {props.types.map(type => {
+          return (type.name + ' ');
         })}</p>
         <p>Peso: {props.pokemon.weight}</p>
         <p>Altura: {props.pokemon.height}</p>
         <Image src={props.pokemon.sprites.front_default} width={200} height={200}></Image>
         <p>Evoluções: </p>
         {props.evolutionChainPokemons.map(pokemon => (
-            <div>
-              <Link href={'/'+pokemon.name}>
-                <a>
-                  <h3>{pokemon.name+' '}</h3>
-                </a>  
-              </Link>
+          <div>
+            <Link href={'/' + pokemon.name}>
+              <a>
+                <h3>{pokemon.name + ' '}</h3>
+              </a>
+            </Link>
 
-            </div>
-          ))}
+          </div>
+        ))}
 
         <Link href="/">
           <a>voltar</a>
