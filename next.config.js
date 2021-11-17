7c85c9e19aa8917e2459230cb2cf70d0004143b2
Franreno <franreno4@gmail.com>
2021-11-17 13:05:50 -0300

    Adding basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,4 +2,6 @@ module.exports = {
     images: {
       domains: ['raw.githubusercontent.com', 'assets.pokemon.com', "upload.wikimedia.org"],
     },
+    basePath: '/pokedex',
+    assetPrefix: '/pokedex'
 } 
